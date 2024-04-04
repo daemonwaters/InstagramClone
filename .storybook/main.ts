b6b8da01550c33de37c9f4b73166afe0e23140f5
Ali Mohammadi <chipleader111@gmail.com>
2024-04-04 16:32:38 +0330

    project initial codebase

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-a11y",
+    "@storybook/addon-viewport",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss'
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <div className="wrapper">
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default App
