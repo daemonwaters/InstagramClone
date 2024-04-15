a9befcfccd207e71d99f1b274f97d92795aa6f36
Ali Mohammadi <chipleader111@gmail.com>
2024-04-15 17:18:41 +0330

    Added Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import './App.scss'
 import SignIn from './pages/SignIn/SignIn'
+import { Routes , Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Inbox from './pages/Inbox/Inbox'
+import Profile from './pages/Profile/Profile'
 
 function App() {
 
   return (
     <React.Fragment>
       <div className="wrapper">
-        <SignIn/>
+        <Routes>
+          <Route element={<SignIn/>} path='/' />
+          <Route element={<Home/>} path='/home' />
+          <Route element={<Inbox/>} path='/inbox' />
+          <Route element={<Profile/>} path='/profile' />
+        </Routes>
       </div>
     </React.Fragment>
   )
